8fd03cefc5150df222ecf42f6c8d3157a203c2a6
Vladimir Malkov <vladimir.malkov@ingka.ikea.com>
2020-03-08 22:09:18 +0300

    first release

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const config = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
   module: {
